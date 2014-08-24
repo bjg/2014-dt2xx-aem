9e5cc515531a747ea78ca493e35a7428ec433dda
bjg <brian.j.gillespie@gmail.com>
2014-08-24 21:34:04 +0100

    Added first Go video lectures

---
+++ slides/golang/list.go
@@ -0,0 +1,19 @@
+package list
+
+type Node {
+    next *Node
+    value int
+}
+
+type List struct {
+    head Node
+}
+
+func (l *List) Add(v int) {
+}
+
+func (l *List) Remove(v int) {
+}
+
+func (l *List) Contains(v int) bool {
+}
